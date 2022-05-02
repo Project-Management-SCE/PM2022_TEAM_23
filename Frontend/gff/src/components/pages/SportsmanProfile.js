99b31523229e3f861fa0465e48e8bdafe1bf3589
Tomer Ben Shimol <tomerbenshimol3164@gmail.com>
2022-05-03 01:26:08 +0300

    Test

--- Frontend/gff/src/components/pages/SportsmanProfile.js
+++ Frontend/gff/src/components/pages/SportsmanProfile.js
@@ -6,7 +6,23 @@ class SportsmanProfile extends React.Component {
 
     constructor(props){
         super(props);
+        this.state =
+        {
+            bmi:'',
+        }
     }
+
+    bmi() {
+        const {user, isAuthenticated, LogIn, LogOut} = this.context;
+        var weight_int = parseInt(user['weight'],10);
+        var height_int = parseFloat(user['height'],10);
+        var bmi = weight_int/(height_int*height_int);
+        console.log(weight_int);
+        console.log(height_int);
+        console.log(bmi);
+        this.setState({bmi: String(bmi)})
+    }
+
 
     render() {
     const {user, isAuthenticated, LogIn, LogOut} = this.context;
@@ -26,7 +42,10 @@ class SportsmanProfile extends React.Component {
                 <br/>
                 <div>Weight: {user['weight']}</div>
                 <br/>
-                <div>BMI:</div>
+                <div>
+                <button type='submit' onClick={() => this.bmi()}>Calc BMI:</button>
+                <label>{this.state.bmi}</label>
+                </div>
                 <br/>
                 <div>Phone Number: {user['phoneNumber']}</div>
                 <br/>
