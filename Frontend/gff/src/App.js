4c79f85ab855b6b5ff0896f51a1f50b58cf01cb8
yuval840 <yuval840@gmail.com>
2022-05-02 11:17:17 +0300

    BSPM2022T23-93

--- Frontend/gff/src/App.js
+++ Frontend/gff/src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 
@@ -8,19 +7,28 @@ import SportsmanForm from './components/forms/SportsmanForm';
 import About from './components/pages/About';
 import CoachForm from './components/forms/CoachForm';
 import LoginForm from './components/forms/LoginForm';
+import {UserProvider} from './UserContext';
+import OurCoaches from './components/pages/OurCoaches';
+import MySportsmans from './components/pages/MySportsmans';
+import SportsmanProfile from './components/pages/SportsmanProfile';
 
 function App() {
   return (
+    <UserProvider>
       <Router>
         <Navbar/>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/sportsman/sign_up" element={<SportsmanForm />} />
-          <Route path="/coach/sign_up" element={<CoachForm />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/sportsman/sign_up" element={<SportsmanForm />} />
+            <Route path="/coach/sign_up" element={<CoachForm />} />
+            <Route path='/publicCoaches' element={<OurCoaches />} />
+            <Route path='/MySportsmans' element={<MySportsmans />} />
+            <Route path='/sportsman/MyProfile' element={<SportsmanProfile />} />
         </Routes>
       </Router>
+      </UserProvider>
   );
 }
 
